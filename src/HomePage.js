f1e4ef07e43db954e43d0d8674da3dc94780bdbf
Takáts Botond <btakats@gmail.com>
2025-04-28 16:24:51 +0200

    képek beillesztése

--- src/HomePage.js
+++ src/HomePage.js
@@ -12,16 +12,31 @@ export function HomePage(){
 
     return <div>
         <h2>Alkotások:</h2>
-        <ul>
-            {alkotasLista.map((alkotas) => (
-                <li key={alkotas.id}>
-                    {alkotas.nev}<br />
-                    {alkotas.alkotasAzonosito}<br />
-                    {alkotas.keletkezesKezdoIdopontjaInt}<br />
-                    {alkotas.tipus}<br />
-                    {alkotas.megjelenitendoNev}<br />
-                </li>
+        {alkotasLista.map((alkotas) => (
+            <div style={{width: "275px", margin: "10px", float: "left"}} key={alkotas.id}>
+                <img src={"https://picsum.photos/275?random=" + alkotas.id} alt="random avatar" /><br />
+                {alkotas.nev} ({alkotas.alkotasAzonosito})<br />
+                {alkotas.keletkezesKezdoIdopontjaInt} - {alkotas.tipus}<br />
+                {alkotas.megjelenitendoNev}<br />
+            </div>
             ))}
-        </ul>
     </div>
 }
+
+/*
+<div class="flex flex-col gap-1 justify-start overflow-hidden flex-1">
+    <div class="w-full aspect-square bg-image-load flex">
+        <a class="min-w-full min-h-full bg-cover bg-center " href="/alkotas/AS0042019" style="background-image: url(&quot;/mma-portal/methods/imageRepository/getImage?key=kPKMwmXDvtGfRGsPadRzXaSTASmeGurgihiu9jxeOZEUzOrYa9&quot;);"></a>
+    </div>
+    <a class="w-full flex flex-col gap-1 mt-1 overflow-hidden " href="/alkotas/AS0042019">
+        <div class="font-slab font-bold text-lg">2024</div>
+        <div class="w-full text-sm">Mohácsi Nemzeti Emlékhely - Múzeum és kápolna</div>
+    </a>
+    <div class="flex flex-col gap-1">
+        <a class="text-sm uppercase hover:underline" href="/alkoto/AL0000475">Vadász Bence</a>
+        <a href="/kereses?kifejezes=Mohács&amp;mezo=telepules&amp;tipus=alkotas&amp;cim=Mohács">
+            <div class="text-xs hover:underline">Mohács</div>
+        </a>
+    </div>
+</div>
+*/
